54991d1d69de584f2a27d9a205ba3f19ed1e17ea
sergio <sergiomartinez_1997@hotmail.com>
2020-11-09 18:14:51 +0100

    Added check for code 200

--- src/main/java/urlshortener/web/UrlShortenerController.java
+++ src/main/java/urlshortener/web/UrlShortenerController.java
@@ -1,19 +1,22 @@
 package urlshortener.web;
 
-import java.net.URI;
-import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import urlshortener.domain.ShortURL;
 import urlshortener.service.ClickService;
 import urlshortener.service.ShortURLService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UrlShortenerController {
@@ -45,7 +48,7 @@ public class UrlShortenerController {
                                             HttpServletRequest request) {
     UrlValidator urlValidator = new UrlValidator(new String[] {"http",
         "https"});
-    if (urlValidator.isValid(url)) {
+    if (urlValidator.isValid(url) && urlAccessible(url)) {
       ShortURL su = shortUrlService.save(url, sponsor, request.getRemoteAddr());
       HttpHeaders h = new HttpHeaders();
       h.setLocation(su.getUri());
@@ -53,6 +56,45 @@ public class UrlShortenerController {
     } else {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+  }
+
+  //Returns true if the url request gives code 200 in the header, otherwise returns false
+  private boolean urlAccessible(String url) {
+    try {
+      URL urlForGet = new URL(url);
+      String readLine = null;
+      HttpURLConnection connection = null;
+      connection = (HttpURLConnection) urlForGet.openConnection();
+      connection.setRequestMethod("GET");
+      int responseCode = connection.getResponseCode();
+
+      if (responseCode == HttpURLConnection.HTTP_OK) {
+        System.out.println("CODE 200");
+        return true;
+      } else {
+        System.out.print("Error Code:");
+        System.out.println(connection.getResponseCode());
+        return false;
+      }
+    } catch (IOException e) {
+      System.out.println("URL not accesible");
+      return false;
+    }
+  }
+
+  //Return a Map with all the info in the header of the request
+  private Map<String, String> getHeadersInfo(HttpServletRequest request) {
+
+    Map<String, String> map = new HashMap<String, String>();
+
+    Enumeration headerNames = request.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String key = (String) headerNames.nextElement();
+      String value = request.getHeader(key);
+      map.put(key, value);
+    }
+
+    return map;
   }
 
   private String extractIP(HttpServletRequest request) {
@@ -64,4 +106,6 @@ public class UrlShortenerController {
     h.setLocation(URI.create(l.getTarget()));
     return new ResponseEntity<>(h, HttpStatus.valueOf(l.getMode()));
   }
+
+
 }
